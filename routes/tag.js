e5e52d6d215aa072b1b9620e0727b7ed5fdd16d2
David Roffe <davidroffe@gmail.com>
2020-10-25 14:55:16 -0400

    Long awaited update to handle jwt and other changes in tag/toggle & tag/delete route

--- routes/tag.js
+++ routes/tag.js
@@ -1,40 +1,36 @@
+const jwt = require('jsonwebtoken');
+const jwtSecret = process.env.JWT_SECRET;
+
 module.exports = (Models, router) => {
-  router.get('/tag/get', async ctx => {
+  router.get('/tag/get', async (ctx) => {
     const allTags = await Models.Tag.findAll();
 
     ctx.body = allTags;
   });
-  router.post('/tag/create', async ctx => {
+  router.post('/tag/create', async (ctx) => {
     const allTags = await Models.Tag.findAll();
 
     ctx.body = allTags;
   });
 
-  router.post(['/tag/toggle', '/tag/delete'], async ctx => {
-    const sessionId = ctx.cookies.get('auth');
-    const tagIds = ctx.query['tagIds[]'];
-
-    if (sessionId) {
-      const user = await Models.User.findOne({
-        where: { sessionId: sessionId }
-      });
-
-      if (user.admin) {
-        if (ctx.url.indexOf('/api/tag/toggle') > -1) {
-          const tags = await Models.Tag.findAll({
-            where: { id: tagIds }
-          });
-          for (var i = 0; i < tags.length; i++) {
-            tags[i].active = !tags[i].active;
-            tags[i].save();
-          }
-        } else {
-          await Models.Tag.destroy({
-            where: { id: tagIds }
-          });
+  router.post(['/tag/toggle', '/tag/delete'], async (ctx) => {
+    const sessionToken = ctx.cookies.get('auth');
+    const payload = sessionToken ? jwt.verify(sessionToken, jwtSecret) : false;
+    const tagIds = ctx.query['tagIds'];
+    console.log(ctx.query);
+    if (payload && payload.admin) {
+      if (ctx.url.indexOf('/api/tag/toggle') > -1) {
+        const tags = await Models.Tag.findAll({
+          where: { id: tagIds },
+        });
+        for (var i = 0; i < tags.length; i++) {
+          tags[i].active = !tags[i].active;
+          tags[i].save();
         }
       } else {
-        ctx.throw(401, 'Unauthorized request.');
+        await Models.Tag.destroy({
+          where: { id: tagIds },
+        });
       }
     } else {
       ctx.throw(401, 'Unauthorized request.');
